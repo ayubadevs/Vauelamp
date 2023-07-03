96d4f160c54ae0484d7d320a6e58d6f458c03de8
Ayuba isiaka <105459982+ayubadevs@users.noreply.github.com>
2023-07-03 17:58:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+  var section = document.getElementById("section");
+  var light = document.getElementById("light");
+  var btn = document.getElementById("btn");
+function toggleBtn(){
+  section.classList.toggle("on");
+};
+
+function toogleButton(){
+    btn.classList.toggle("active");
+    light.classList.toggle("onn");
+}
+
+
